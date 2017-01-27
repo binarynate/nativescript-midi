861a8ae7d06e9637820e252e4dc3892414503d37
Nathan Hill <nathan.hill@inin.com>
2017-01-26 21:50:24 -0500

    Got the plugin to correctly instantiate an Android MidiManager.

--- dist/android/AndroidMidiDeviceManager.js
+++ dist/android/AndroidMidiDeviceManager.js
@@ -1,10 +1,18 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /* global android */
+
+var _application = require('application');
+
+var _application2 = _interopRequireDefault(_application);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -18,18 +26,22 @@ var AndroidMidiDeviceManager = function () {
     }
 
     _createClass(AndroidMidiDeviceManager, [{
-        key: "addDeviceAddedListener",
+        key: 'addDeviceAddedListener',
         value: function addDeviceAddedListener() {}
     }, {
-        key: "addDeviceRemovedListener",
+        key: 'addDeviceRemovedListener',
         value: function addDeviceRemovedListener() {}
     }, {
-        key: "addDeviceUpdatedListener",
+        key: 'addDeviceUpdatedListener',
         value: function addDeviceUpdatedListener() {}
     }, {
-        key: "getDevices",
+        key: 'getDevices',
         value: function getDevices() {
+            var MIDI_SERVICE = android.content.Context.MIDI_SERVICE;
 
+            var midiManager = _application2.default.android.currentContext.getSystemService(MIDI_SERVICE);
+
+            console.log('typeof midiManager.getDevices: ' + _typeof(midiManager.getDevices));
             return new Promise(function (resolve) {
                 return setTimeout(resolve, 2000);
             }).then(function () {
--- src/android/AndroidMidiDeviceManager.js
+++ src/android/AndroidMidiDeviceManager.js
@@ -1,3 +1,8 @@
+/* global android */
+
+import app from 'application';
+
+
 /**
 * Responsible for fetching available MIDI devices and notifying the application of device changes
 * on the Android platform.
@@ -18,6 +23,10 @@ class AndroidMidiDeviceManager {
 
     getDevices() {
 
+        let { MIDI_SERVICE } = android.content.Context;
+        let midiManager = app.android.currentContext.getSystemService(MIDI_SERVICE);
+
+        console.log(`typeof midiManager.getDevices: ${typeof midiManager.getDevices}`);
         return (new Promise(resolve => setTimeout(resolve, 2000))).then(() => []);
     }
 }
